ee97d4c2178ea057f24a74263929cac2a2fe50c0
袁博 <thamiti@163.com>
2019-04-12 11:38:33 +0800

    Signed-off-by: 袁博 <thamiti@163.com>

---
+++ config.js
@@ -0,0 +1,10 @@
+//代理配置
+let conifg = {
+    '/api/':{
+        target: 'http://bayin.mycwgs.com',
+    },
+    '/osm/':{
+        target: 'http://120.79.90.199:80',
+    }
+}
+module.exports = conifg
